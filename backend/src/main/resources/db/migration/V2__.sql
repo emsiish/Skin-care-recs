ae4625061f7665425947436d22e7fd7816312a87
Емилия Ушева <emiliyausheva730@gmail.com>
2024-03-01 00:57:43 +0200

    updated doctor migrations

--- backend/src/main/resources/db/migration/V2__.sql
+++ backend/src/main/resources/db/migration/V2__.sql
@@ -5,5 +5,6 @@ CREATE TABLE doctor
     phone_number VARCHAR(255)          NULL,
     email        VARCHAR(255)          NULL,
     hospital     VARCHAR(255)          NULL,
+    image        VARCHAR(255)          NULL,
     CONSTRAINT pk_doctor PRIMARY KEY (id)
 );
